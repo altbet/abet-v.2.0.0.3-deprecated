2ec8b8fe9cbbd493a08ddb6123f9a55de5d36887
Hendra Wuberry <16115109+j10da@users.noreply.github.com>
2019-04-06 12:13:51 +0700

    Update build-win64.sh

--- scripts/build-win64.sh
+++ scripts/build-win64.sh
@@ -6,7 +6,7 @@ cd ..
 
 ./autogen.sh # not required when building from tarball
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/
-make
+sudo make
 cd src
 cd qt
 strip altbet-qt.exe
