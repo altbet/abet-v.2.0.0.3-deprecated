3457bfa771f7836cbb1415c3d07a4cdba4450726
TFinch <travisfinch01@gmail.com>
2019-04-12 16:02:32 -0400

    Revert "Collat Test"
    This reverts commit 1fe3f6cdf29788e6ab272c0e777c86e01f089cfd.

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -24,7 +24,7 @@ CAmount GetMasternodeCollateral()
 	{
 		return Params().MasternodeCollateralAmtNew();
 	} else {
-		return Params().MasternodeCollateralAmt() || Params().MasternodeCollateralAmtNew();
+		return Params().MasternodeCollateralAmt();
 	}
 	
 }
